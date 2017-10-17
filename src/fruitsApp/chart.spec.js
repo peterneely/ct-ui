df6f5578be3b515c96031bb0655b29f02bbd0001
Peter Neely <Peter@DialMBPR.local>
2017-10-17 10:16:57 -0700

    refactor test

--- src/fruitsApp/chart.spec.js
+++ src/fruitsApp/chart.spec.js
@@ -2,32 +2,33 @@ import ChartData from './ChartData';
 
 describe('ChartData', () => {
 
+  const people = [
+    { name: 'Dennis T.', favoriteFruit: 'grapes' },
+    { name: 'Vlad S.', favoriteFruit: 'banana' },
+    { name: 'Javier F.', favoriteFruit: 'banana' },
+    { name: 'Aaron C.', favoriteFruit: 'grapes' },
+    { name: 'Ruby O.', favoriteFruit: 'orange' },
+    { name: 'Paco D.', favoriteFruit: 'strawberries' },
+    { name: 'Francis P.', favoriteFruit: 'orange' },
+    { name: 'Edward J.', favoriteFruit: 'orange' },
+    { name: 'Ethan J.', favoriteFruit: 'grapes' },
+    { name: 'Wanda T.', favoriteFruit: 'orange' },
+    { name: 'Jason T.', favoriteFruit: 'grapes' },
+  ];
+  const fruits = [
+    { fruitName: 'grapes', count: 4 },
+    { fruitName: 'orange', count: 4 },
+    { fruitName: 'banana', count: 2 },
+    { fruitName: 'strawberries', count: 1 },
+  ];
+  const barWidths = {
+    grapes: '36%',
+    orange: '36%',
+    banana: '18%',
+    strawberries: '9%',
+  };
+
   it('can parse fruits correctly', () => {
-    const people = [
-      { name: 'Dennis T.', favoriteFruit: 'grapes' },
-      { name: 'Vlad S.', favoriteFruit: 'banana' },
-      { name: 'Javier F.', favoriteFruit: 'banana' },
-      { name: 'Aaron C.', favoriteFruit: 'grapes' },
-      { name: 'Ruby O.', favoriteFruit: 'orange' },
-      { name: 'Paco D.', favoriteFruit: 'strawberries' },
-      { name: 'Francis P.', favoriteFruit: 'orange' },
-      { name: 'Edward J.', favoriteFruit: 'orange' },
-      { name: 'Ethan J.', favoriteFruit: 'grapes' },
-      { name: 'Wanda T.', favoriteFruit: 'orange' },
-      { name: 'Jason T.', favoriteFruit: 'grapes' },
-    ];
-    const fruits = [
-      { fruitName: 'grapes', count: 4 },
-      { fruitName: 'orange', count: 4 },
-      { fruitName: 'banana', count: 2 },
-      { fruitName: 'strawberries', count: 1 },
-    ];
-    const barWidths = {
-      grapes: '36%',
-      orange: '36%',
-      banana: '18%',
-      strawberries: '9%',
-    };
     const chartData = new ChartData(people);
     expect(chartData.parse).toBeDefined();
     const { calcBarWidths, orderedFruits } = chartData.parse();
